e9fa55c643be579031141b9a43267a74acca4477
Nathan Nahi <nathannahisteam@gmail.com>
2023-05-28 02:07:55 +1000

    Start modifying data

--- src/types.ts
+++ src/types.ts
@@ -5,10 +5,28 @@ export interface Point {
 
 export const enum BaseSpace {
   EmptySpace = -1,
-  wall = 0
+  wall = 0,
 }
 
 export const enum PlayerStance {
   Standing = "standing",
-  Sitting = "sitting"
+  Sitting = "sitting",
+}
+
+export interface Item {
+  id: string;
+  name: string;
+  description: string;
+}
+
+export interface Location {
+  id: string;
+  name: string;
+  description: string;
+  descriptionDistant: string;
+  descriptionNorth?: string;
+  descriptionEast?: string;
+  descriptionSouth?: string;
+  descriptionWest?: string;
+  itemsInitial: string[];
 }
